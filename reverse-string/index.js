9a4d5076f89e88bab297127f5fc9bf464215cedd
Ramón Miklus <ramon.miklus@gmail.com>
2019-03-10 00:00:36 +0100

    provided second solution for reverse string

--- reverse-string/index.js
+++ reverse-string/index.js
@@ -3,12 +3,24 @@
 // order of characters
 // --- Examples
 //   reverse('apple') === 'leppa'
-//   reverse('hello') === 'olleh'
-//   reverse('Greetings!') === '!sgniteerG'
+//   reverse('hello ') === ' olleh'
 
-// -- Solution
-// we can use the reverse() method available on the Array prototype. Hence we need to convert the string to array, 
-// reverse it, and cast it to a string again
+// -- Solution 1
+// we can use the reverse() method available on the Array prototype. Hence we need to convert the string into an array, 
+// reverse it, and cast it to string
 const reverseString = (str) => str.split('').reverse().join('')
+
+
+// -- Solution 2
+// avoid using reverse() so loop through the string manually
+// const reverseString = (str) => {
+//    let reversed = '';
+
+//    for (let character of str) {
+//        reversed = character + reversed
+//    }
+
+//    return reversed;
+// }
 
 module.exports = reverseString;
