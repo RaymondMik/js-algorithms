84f7c63ec01e8b71a05b56f190f37a1b1ff7f714
Ramón Miklus <ramon.miklus@gmail.com>
2019-03-10 00:11:18 +0100

    provided third solution for reverse string

--- reverse-string/index.js
+++ reverse-string/index.js
@@ -8,11 +8,10 @@
 // -- Solution 1
 // we can use the reverse() method available on the Array prototype. Hence we need to convert the string into an array, 
 // reverse it, and cast it to string
-const reverseString = (str) => str.split('').reverse().join('')
-
+//const reverseString = (str) => str.split('').reverse().join('')
 
 // -- Solution 2
-// avoid using reverse() so loop through the string manually
+// avoid using reverse(), use a for loop instead
 // const reverseString = (str) => {
 //    let reversed = '';
 
@@ -22,5 +21,9 @@ const reverseString = (str) => str.split('').reverse().join('')
 
 //    return reversed;
 // }
+
+// -- Solution 3
+// avoid using reverse(), use reduce() instead
+const reverseString = (str) => str.split('').reduce((acc, character) => character + acc, '')
 
 module.exports = reverseString;
