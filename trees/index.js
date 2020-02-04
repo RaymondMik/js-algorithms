fdaec745f27844835d694d0acbabf35d35766b62
Ramón Miklus <ramon.miklus@gmail.com>
2020-02-04 09:21:35 +0100

    tree traversal

--- trees/index.js
+++ trees/index.js
@@ -37,7 +37,9 @@ class Tree {
       while(nodes.length) {
          // remove the first node and store it in firstNode
          let firstNode = nodes.shift();
+         // add node children at the end of the array
          nodes.push(...firstNode.children);
+         // run fun on the node
          fun(firstNode);
       }
    }
@@ -46,24 +48,74 @@ class Tree {
       let nodes = [this.root];
 
       while(nodes.length) {
+         // remove the first node and store it in firstNode
          let firstNode = nodes.shift();
+         // add node children at the beginning of the array
          nodes.unshift(...firstNode.children);
+         // run fun on the node
          fun(firstNode);
       }
    }
 }
 
-// const myTree = new Tree();
-// myTree.root = new Node(20);
-// myTree.root.add(0);
-// myTree.root.add(40);
-// myTree.root.add(-15);
-// myTree.root.children[0].add(12);
-// myTree.root.children[0].add(-2);
-// myTree.root.children[0].add(1);
-// myTree.root.children[2].add(-2);
+const myTree = new Tree();
+myTree.root = new Node(20);
+myTree.root.add(0);
+myTree.root.add(40);
+myTree.root.add(-15);
+myTree.root.children[0].add(12);
+myTree.root.children[0].add(-2);
+myTree.root.children[0].add(1);
+myTree.root.children[2].add(-2);
 
 // myTree.breadthFirstTraversal((node) => node.data = node.data + 1);
 // myTree.depthFirstTraversal((node) => node.data = node.data + 1);
 
-module.exports = { Tree, Node };
+/******** LEVEL WIDTH EXERCISE ********/
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+function levelWidth(root) {
+   let levels = [0];
+   let nodes = [root, 'STOPPER'];
+
+   while(nodes.length > 1) {
+      // [Node, "STOPPER"]
+      // remove the first node and store it in firstNode
+      let firstNode = nodes.shift();
+      // ["STOPPER"]
+      if (firstNode && firstNode !== 'STOPPER' && firstNode.children) {
+         // add node children at the end of the array to mark a new leve
+         nodes.push(...firstNode.children);
+         // ["STOPPER", ChildNode, ChildNode, ...]
+      }
+      
+      // increment levels
+      levels[levels.length - 1]++;
+      // move stopper value to the end of nodes, this helps us figuring out we processed an entire level
+      if (nodes[0] === 'STOPPER' && nodes.length > 1) {
+         const stopper = nodes.shift();
+         nodes.push(stopper);
+         // [ChildNode, ChildNode, ... , "STOPPER"]
+
+         // move to the next level of the tree
+         levels.push(0);
+      }
+   }
+
+   return levels;
+}
+
+levelWidth(myTree.root)
+
+module.exports = { Tree, Node, levelWidth };
--- trees/test.js
+++ trees/test.js
@@ -1,6 +1,7 @@
 const T = require('./index');
 const Node = T.Node;
 const Tree = T.Tree;
+const levelWidth = T.levelWidth;
 
 describe('Node', () => {
   test('Node is a constructor', () => {
@@ -63,5 +64,32 @@ describe('Tree', () => {
     });
 
     expect(letters).toEqual(['a', 'b', 'c', 'd']);
+  });
+});
+
+describe('Level Width', () => {
+  test('levelWidth is a function', () => {
+    expect(typeof levelWidth).toEqual('function');
+  });
+  
+  test('levelWidth returns number of nodes at widest point', () => {
+    const root = new Node(0);
+    root.add(1);
+    root.add(2);
+    root.add(3);
+    root.children[0].add(4);
+    root.children[2].add(5);
+  
+    expect(levelWidth(root)).toEqual([1, 3, 2]);
+  });
+  
+  test('levelWidth returns number of nodes at widest point', () => {
+    const root = new Node(0);
+    root.add(1);
+    root.children[0].add(2);
+    root.children[0].add(3);
+    root.children[0].children[0].add(4);
+  
+    expect(levelWidth(root)).toEqual([1, 1, 2, 1]);
   });
 });
